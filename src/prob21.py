b5cda11ce809aa91ebac0633faea5780c3399ba9
Jared Scott <jaredcscott93@aggiemail.usu.edu>
2019-04-16 07:19:48 -0600

    prob 21, 15

--- src/prob21.py
+++ src/prob21.py
@@ -20,22 +20,40 @@ sum = 0
 
 
 
-def sumFactors(factors):
+def sumFactors(factors, num):
     sum = 0
-    for j in curfactors:
-        if j * j == 284:
+    for j in factors:
+        if j * j == num:
             continue
         else:
             sum += j
     return sum
           
-for i in range(10000):
-    curfactors = print_factors(i)
-    otherNum = sumFactors(curfactors)
-    otherFactors = print_factors(otherNum)
-    otherSum = sumFactors(otherNum)
-    if sumFactors(curfactors) ==  otherSum:
-        print('amicable', sumFactors(curfactors), otherSum) 
+#for i in range(285):
+#    curFactors = print_factors(i)
+#    print(sumFactors(curFactors, i),' ', i)
+    
+    #otherNum = sumFactors(curfactors)
+    #otherFactors = print_factors(otherNum)
+    #otherSum = sumFactors(otherNum)
+    #if sumFactors(curfactors) ==  otherSum:
+    #    print('amicable', sumFactors(curfactors), otherSum) 
         
+#curFactors = print_factors(284)
+#print(sumFactors(curFactors, 284),' ', 284) # 220 284 
+#curFactors1 = print_factors(220)
+#print(sumFactors(curFactors1, 220), ' ', 220) # 284 220
+#if sumFactors(curFactors1, 220) == 284 and sumFactors(curFactors, 284) == 220:
+#            print("here")
+sum = 0
+for i in range(1,10001):
+    curFactors = print_factors(i)
+    for j in range(i + 1):
+        curFactors1 = print_factors(j)
+        if sumFactors(curFactors1, j) == i and sumFactors(curFactors, i) == j and sumFactors(curFactors, i) != i:
+            sum += i
+            sum += j
+            print(i, ' ',j)
+            print("current sum: ", sum)
 
-      
+print(sum)      
