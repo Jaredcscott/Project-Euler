c7b57128e25e2012e3bdc67c8a7e8236ad4440f5
Jared Scott <jaredcscott93@aggiemail.usu.edu>
2019-06-23 19:24:20 -0600

    adding prob 15

--- src/prob15.py
+++ src/prob15.py
@@ -1,11 +1,20 @@
-grid = [['a','b','c'],['d','e','f'],['g','h','i']]
-start = grid[0][0]
-for y in range(len(grid)):
-    for x in range(len(grid[0])):
-        print(grid[y][x])
+#grid = [['a','b','c'],['d','e','f'],['g','h','i']]
+#start = grid[0][0]
+#for y in range(len(grid)):
+#    for x in range(len(grid[0])):
+#        print(grid[y][x])
 
 
-for i in grid:
-    print(i)
+#for i in grid:
+#    print(i)
 
 #print(start)
+
+gridSize = 20 
+numOfPaths = 1
+ 
+for i in range(gridSize):
+    numOfPaths *= (2 * gridSize) - i
+    numOfPaths /= i + 1
+    
+print(numOfPaths)
