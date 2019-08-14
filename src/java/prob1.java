35b54fe6416e98ca5cbeea2e54240ba356d4f932
jared work <jaredcscott93@gmail.com>
2019-08-14 08:55:22 -0600

    resolved prob1 with java

---
+++ src/java/prob1.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+public class prob1 {
+	public static void main(String[] args) {
+		ArrayList<Integer> nums = new ArrayList<Integer>();
+		for (int i = 0; i < 1000; i++) {
+			if (i % 3 == 0 || i % 5 == 0) {
+				nums.add(i);
+			}
+		}
+		int sum = 0;
+		for (int num : nums) {
+			sum += num;
+		}
+		System.out.println(sum);
+	}
+}
