1abab30b881e478cc24ace985943b4c29debda32
Jared Scott <jareds@Dutro.local>
2019-12-17 16:20:33 -0700

    adding in problem 32

--- src/java/prob32.java
+++ src/java/prob32.java
@@ -1,6 +1,7 @@
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Hashtable;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -13,36 +14,73 @@ import java.util.Arrays;
  * @author jareds
  */
 public class prob32 {
-    public static String isPandigital(Long num) {
+    public static boolean isPandigital(Long num) {
         boolean dupNums = false;
         String numString = "" + num;
-        boolean[] nums = new boolean[10];
-        if ( numString.length() == 10 ) {
-            for (int i = 0 ; i <numString.length(); i ++) {
-                char c = numString.charAt(i);
+        boolean[] nums = new boolean[9];
+        if ( numString.length() == 9 ) {
+            for (int i = 1 ; i <= numString.length(); i++) {
+                char c = numString.charAt(i-1);
                 Integer index = Character.getNumericValue(c);
-                if(!nums[index]) {
-                    nums[index] = true;
+                if (index == 0) {
+                    return false;
+                }
+                //System.out.println(index);
+                if(!nums[index-1]) {
+                    nums[index-1] = true;
                 }
                 else {
+                    //System.out.println(index);
                     dupNums = true;
                 }
             }
         }
         else {
-            return "Failure!";
+            return false; //"Failure!"
         }
-        return !dupNums ? "Sucess! " + numString : "Failure!";
+        return !dupNums ; //? "Sucess! " + numString : "Failure!"
     }
+    
+    
     public static void main(String[] args) {
         ArrayList<Long> numsToCheck = new ArrayList<Long>();
         numsToCheck.add(2348157690l);
-        numsToCheck.add(1264538779l);
-        numsToCheck.add(234815590l);
+        numsToCheck.add(217843568l);
+        numsToCheck.add(391867254l);
+        int sum = 0;
+        ArrayList<Integer> numsFound = new ArrayList<Integer>();
+        for (int i = 1; i < 10000; i++) {
+            for (int j = 0 ; j < 10000; j++) {
+                int num = i * j;
+                String numS = "" + i + "" + j + "" + num;
+//                if(i == 39 && j == 186) {
+//                    System.out.println(isPandigital(Long.parseLong(numS)));
+//                }
+                if (numS.length() == 9) {
+                    if( isPandigital(Long.parseLong(numS)) ) {
+                        //System.out.println(Long.parseLong(numS));
+                        if (!numsFound.contains(num)) {
+                            sum += num;
+                            numsFound.add(num);
+                        }
+                        else {
+                            //System.out.println(num);
+                        }
+                        
+                        //sum += j;
+                        //System.out.println(Long.parseLong(numS));
+                    }
+                    //System.out.println(Long.parseLong(numS));
+                    //System.out.println("i: " + i + " j: " + j + " product: " + num + "\n\n\n");
+                } 
+            }
+        }
+        System.out.println(sum);
         
-        
-        for(Long num : numsToCheck) {
-            System.out.println(isPandigital(num));
-        }   
+//        
+//        
+//        for(Long num : numsToCheck) {
+//            System.out.println(isPandigital(num));
+//        }   
     }
 }
