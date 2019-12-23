31332a4fccd68c6e125939bb17f3fedd202119ac
Jared <jaredcscott93@gmail.com>
2019-12-23 07:50:05 -0700

    finished prob 89

--- src/java/Project_euler/src/prob89.java
+++ src/java/Project_euler/src/prob89.java
@@ -14,18 +14,33 @@ import java.util.Scanner;
  */
 public class prob89 {
     
-    public static int getValue(char letter) {
-        switch (letter) { 
+    public static int getValue(char letter1, char letter2) {
+        switch (letter1) { 
             case 'I': 
-                return 1; 
+                if (letter2 == 'V' || letter2 == 'X') {
+                   return -1;
+                }
+                else {
+                    return 1;
+                }
             case 'V': 
                 return 5; 
-            case 'X': 
-                return 10; 
+            case 'X':
+                if (letter2 == 'L' || letter2 == 'C') {
+                    return -10;
+                }
+                else {
+                   return 10; 
+                }
             case 'L': 
                 return 50;  
-            case 'C': 
-                return 100; 
+            case 'C':
+                if (letter2 == 'D' || letter2 == 'M') {
+                    return -100;
+                }
+                else {
+                    return 100;
+                } 
             case 'D': 
                 return 500; 
             case 'M': 
@@ -40,24 +55,39 @@ public class prob89 {
                 minimal.append('M');
                 value -= 1000;
             }
+            if (value >= 900) {
+                minimal.append("CM");
+                value -= 900;
+            }
             while (value >= 500) {
                 minimal.append('D');
                 value -= 500;
+            }
+            if (value >= 400) {
+                minimal.append("CD");
+                value -= 400;
             }
             while (value >= 100) {
                 minimal.append('C');
                 value -= 100;
             }
+            if (value >= 90) {
+                minimal.append("XC");
+                value -= 90;
+            }
             while (value >= 50) {
                 minimal.append('L');
                 value -= 50;
             }
+            if (value >= 40) {
+                minimal.append("XL");
+                value -= 40;
+            }
             while (value >= 10) {
-                
                 minimal.append('X');
                 value -= 10;
             }
-            if(value == 9) {
+            if(value >= 9) {
                 minimal.append("IX");
                 value -= 9;
             }
@@ -65,7 +95,7 @@ public class prob89 {
                 minimal.append('V');
                 value -= 5;
             }
-            if (value == 4) {
+            if (value >= 4) {
                 minimal.append("IV");
                 value -= 4;
             }
@@ -81,56 +111,27 @@ public class prob89 {
         String attempt = "IIIX";
         File file = new File("src/prob89.txt");
         Scanner in = new Scanner(file);
+        int totalSaved = 0;
         int charSaved = 0;
         while (in.hasNextLine()) {
             String line = in.nextLine();
             int lineLength = line.length();
             int lineSum = 0;
             for (int i = 0; i < line.length(); i++) {
-                lineSum += getValue(line.charAt(i));
+                if (i == line.length() - 1) {
+                    lineSum += getValue(line.charAt(i), 'E');
+                }
+                else {
+                   lineSum += getValue(line.charAt(i), line.charAt(i + 1)); 
+                }
             }
-            
-//            while (lineSum >= 1000) {
-//                minimal.append('M');
-//                lineSum -= 1000;
-//            }
-//            while (lineSum >= 500) {
-//                minimal.append('D');
-//                lineSum -= 500;
-//            }
-//            while (lineSum >= 100) {
-//                minimal.append('C');
-//                lineSum -= 100;
-//            }
-//            while (lineSum >= 50) {
-//                minimal.append('L');
-//                lineSum -= 50;
-//            }
-//            while (lineSum >= 10) {
-//                
-//                minimal.append('X');
-//                lineSum -= 10;
-//            }
-//            if(lineSum == 9) {
-//                minimal.append("IX");
-//                lineSum -= 9;
-//            }
-//            else if (lineSum == 8) {
-//                minimal.append("IIX");
-//                lineSum -= 8;
-//            }
-//            while (lineSum >= 5) {
-//                minimal.append('V');
-//                lineSum -= 5;
-//            }
-//            while (lineSum >= 1) {
-//                minimal.append('I');
-//                lineSum -= 1;
-//            }
-            
-            charSaved = lineLength; // - minimalLength
-            System.out.println(line + " Value: " + lineSum + "  :  " + minimize(line, lineSum));
+            String minimal = minimize(line, lineSum); 
+            int minimalLength = minimal.length();
+            charSaved = lineLength - minimalLength; //
+            totalSaved += charSaved;
+            System.out.println(line + " Value: " + lineSum + "  :  " + minimize(line, lineSum) + " Char saved: " + charSaved);
         }
+        System.out.println(totalSaved);
     }
     
 }
